12c5672cc835a9c7be8427ef149fdff9f796b6fb
pblo <pdiazarenas541@gmail.com>
2025-10-06 00:54:36 +0100

    icon sawp

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import Button from "react-bootstrap/Button";
 
 const MyButton = (props) => {
-  return <Button variant={props.btnColor} onClick={props.clickAction}>{props.btnText}</Button>;
+  return <Button className="fw-bolder" variant={props.btnColor} onClick={props.clickAction}>{props.btnText}</Button>;
 };
 
 export default MyButton;
